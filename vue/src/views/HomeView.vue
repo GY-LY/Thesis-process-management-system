c2af355eacec5dc9b26837e9c31b8178095f3927
20201103038 <2059462514@qq.com>
2024-01-04 20:50:23 +0800

    vue

---
+++ vue/src/views/HomeView.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="home">
+  <h1>{{ msg }}</h1>
+    <button color="danger" >{{ msg }}</button>
+    <el-button>Default</el-button>
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'HomeView',
+  components: {
+    HelloWorld
+  },
+
+  data () {
+    return {
+      msg: "hello 高帅"
+    }
+  }
+
+
+
+}
+</script>
